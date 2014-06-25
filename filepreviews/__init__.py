2367f4be8eed3ccc10fe38f1902f6ab066ae9131
José Padilla <jpadilla@webapplicate.com>
2014-06-25 11:32:29 -0400

    Update to work with latest API changes

--- filepreviews/__init__.py
+++ filepreviews/__init__.py
@@ -3,19 +3,17 @@ import logging
 
 try:
     # Python 3
-    from urllib.parse import urlencode
     from urllib.error import HTTPError
     from urllib.request import urlopen, Request
 except ImportError:
     # Python 2
-    from urllib import urlencode
     from urllib2 import Request, urlopen, HTTPError
 
 
-__version__ = '1.0.7'
+__version__ = '1.1.0'
 VERSION = __version__
 
-API_URL = 'https://api.filepreviews.io/v1'
+API_URL = 'https://api.filepreviews.io/v1/'
 
 logger = logging.getLogger('filepreviews')
 
@@ -34,14 +32,8 @@ class FilePreviews(object):
     def generate(self, url, **kwargs):
         logger.debug('Generating preview for {}'.format(url))
 
-        request_url = self._generate_request_url(url, **kwargs)
-
         try:
-            request = Request(request_url)
-
-            if self.api_key:
-                request.add_header('X-API-Key', self.api_key)
-
+            request = self._generate_request(url, **kwargs)
             response = urlopen(request)
             data = self._get_json_response(response)
         except HTTPError as response:
@@ -52,15 +44,13 @@ class FilePreviews(object):
             'preview_url': data['preview_url']
         }
 
-    def _generate_request_url(self, url, **kwargs):
+    def _generate_request(self, url, **kwargs):
         metadata = set(kwargs.pop('metadata', []))
         size = kwargs.pop('size', None)
         params = {
-            'url': url
+            'url': url,
+            'metadata': list(metadata)
         }
-
-        if metadata:
-            params['metadata'] = ','.join(metadata)
 
         if size:
             width = size.get('width')
@@ -73,11 +63,22 @@ class FilePreviews(object):
             if height:
                 geometry = '{}x{}'.format(geometry, height)
 
-            params['size'] = geometry
+            params['sizes'] = [geometry]
 
         params.update(kwargs)
 
-        return '{}/?{}'.format(self.api_url, urlencode(params))
+        headers = {
+            'Content-Type': 'application/json',
+        }
+
+        data = json.dumps(params).encode('utf-8')
+
+        request = Request(self.api_url, data, headers)
+
+        if self.api_key:
+            request.add_header('X-API-Key', self.api_key)
+
+        return request
 
     def _poll_metadata(self, metadata_url, **kwargs):
         logger.debug('Polling {}'.format(metadata_url))
